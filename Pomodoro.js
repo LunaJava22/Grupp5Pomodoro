21360b0b691d0b73252ecfabd8d6bcace9082fcd
LunaJava22 <117752558+LunaJava22@users.noreply.github.com>
2022-12-06 14:59:05 +0100

    Update Pomodoro.js

--- Pomodoro.js
+++ Pomodoro.js
@@ -17,5 +17,46 @@ function secondsCount() {
 
     secondsArea.innerText = seconds;
     minutesArea.innerText = minutes;
+    
+    var mixBut = document.getElementById("mixBut");
+
+mixBut.addEventListener("click", Start);
+
+function Start(){
+    console.log("Started");
+    mixBut.removeEventListener("click", Start);
+    mixBut.addEventListener("click", Stop);
+    mixBut.value = "Stop";
 }
+
+function Stop(){
+    console.log("Stopped");
+    mixBut.removeEventListener("click", Stop);
+    mixBut.addEventListener("click", Start);
+    mixBut.value = "Start";
+}
+    
+<button id="start">start</button>
+<button id="stop">stop</button>
+    let start = document.querySelector('#start');
+let stop  = document.querySelector('#stop');
+    start.addEventListener('click', function() {
+	let i = 0;
+	
+	let timerId = setInterval(function() {
+		console.log('!')
+	}, 1000);
+});
+    start.addEventListener('click', function() {
+	let i = 0;
+	
+	let timerId = setInterval(function() {
+		console.log('!')
+	}, 1000);
+});
+
+// Stopping the timer:
+stop.addEventListener('click', function() {
+	clearInterval(timerId);
+});
 
